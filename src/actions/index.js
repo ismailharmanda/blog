939ed23f8fad9bfb74097842b1a3a3a896bf34ca
ismail harmanda <iharmanda@hotmail.com>
2021-07-04 16:55:44 +0300

    Datas has been fetched via API (async)

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,6 @@ export const fetchPosts = () => {
   return async (dispatch, getState) => {
     const response = await jsonPlaceholder.get("/posts");
 
-    dispatch({ type: "FETCH_POSTS", payload: response });
+    dispatch({ type: "FETCH_POST", payload: response.data });
   };
 };
